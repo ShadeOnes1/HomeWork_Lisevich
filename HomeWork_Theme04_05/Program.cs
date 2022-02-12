1fa87783daf4209b682dfd2acbf3ef8b95fb326b
Nikita Lisevich <nikitalisev@gmail.com>
2022-02-12 21:49:52 +0300

    Дополнил

---
+++ HomeWork_Theme04_05/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HomeWork_Theme04_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 2");
+
+            Console.WriteLine("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+            
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[i + 1];
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = Fact(i) / (Fact(j) * Fact(i - j));
+                }
+            }
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static int Fact(int value)
+        {
+            int fact = 1;
+
+            for(int i = 1; i <= value; i++)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
