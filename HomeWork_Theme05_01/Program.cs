a93b1c9e62f66d9385dd8d52907dfa0224ce1a7d
Nikita Lisevich <nikitalisev@gmail.com>
2022-02-27 20:18:38 +0300

    Домашнее задание по теме 5.

---
+++ HomeWork_Theme05_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork_Theme05_01
+{
+    class Program
+    {
+        static string[] DataSeparation(string sourceString) 
+        {
+            return sourceString.Split(' ');
+        }
+
+        static void Output(string[] separatedString)
+        {
+            for (int i = 0; i < separatedString.Length; i++)
+            {
+                Console.WriteLine($"{separatedString[i]}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1. Метод разделения строки на слова");
+            Console.WriteLine("Введите предложение: ");
+
+            string sourceString = Console.ReadLine();
+
+            string[] separatedString = DataSeparation(sourceString);
+            Output(separatedString);
+
+            Console.ReadKey();
+        }
+    }
+}
