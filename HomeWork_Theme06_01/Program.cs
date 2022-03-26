c8ff02a8e07be5c4112c855eaf493877e9036a0d
Nikita Lisevich <nikitalisev@gmail.com>
2022-03-26 21:56:06 +0300

    ДЗ тема 6

--- HomeWork_Theme06_01/Program.cs
+++ HomeWork_Theme06_01/Program.cs
@@ -6,42 +6,35 @@ namespace HomeWork_Theme06_01
 {
     class Program
     {
-        static void Input()
+        static void Input(int ID)
         {
+
             using (StreamWriter sw = new StreamWriter("data.txt", true))
             {
-                char key = '2';
-                int ID = 0;
+                string note = string.Empty;
 
-                do
-                {
-                    string note = string.Empty;
+                note += $"{ID}#";
 
-                    ID = ID + 1;
-                    note += $"{ID}#";
+                Console.Write("\nВведите ФИО сотрудника ");
+                note += $"{Console.ReadLine()}#";
 
-                    Console.Write("\nВведите ФИО сотрудника ");
-                    note += $"{Console.ReadLine()}#";
+                string now = DateTime.Now.ToString();
+                Console.WriteLine($"\nДата добавления записи {now}");
+                note += $"{now}#";
 
-                    string now = DateTime.Now.ToString();
-                    Console.WriteLine($"\nДата добавления записи {now}");
-                    note += $"{now}#";
+                Console.Write("\nВведите возраст сотрудника ");
+                note += $"{Console.ReadLine()}#";
 
-                    Console.Write("\nВведите возраст сотрудника ");
-                    note += $"{Console.ReadLine()}#";
+                Console.Write("\nВведите рост сотрудника ");
+                note += $"{Console.ReadLine()}#";
 
-                    Console.Write("\nВведите рост сотрудника ");
-                    note += $"{Console.ReadLine()}#";
+                Console.Write("\nВведите дату рождения сотрудника ");
+                note += $"{Console.ReadLine()}#";
 
-                    Console.Write("\nВведите дату рождения сотрудника ");
-                    note += $"{Console.ReadLine()}#";
+                Console.Write("\nВведите место рождения сотрудника ");
+                note += $"{Console.ReadLine()}#";
 
-                    Console.Write("\nВведите место рождения сотрудника ");
-                    note += $"{Console.ReadLine()}#";
-
-                    sw.WriteLine(note);
-                    Console.Write("Продожить 1/2?"); key = Console.ReadKey(true).KeyChar;
-                } while (char.ToLower(key) == '2');
+                sw.WriteLine(note);
             }
         }
 
@@ -64,7 +57,29 @@ namespace HomeWork_Theme06_01
         {
             Console.WriteLine("Сотрудники");
 
+            bool isExitInput = false;
 
+            int ID = 1;
+
+            while (!isExitInput)
+            {
+                Console.WriteLine("Команды:\n1 - Чтение из файла;\n2 - Запись в файл;\n <любой символ> - выход");
+                string inputedLine = Console.ReadLine();
+
+                switch (inputedLine)
+                {
+                    case "1":
+                        Read();
+                        break;
+                    case "2":
+                        Input(ID);
+                        ID++;
+                        break;
+                    default:
+                        isExitInput = true;
+                        break;
+                }
+            }
         }
-    }
+    }    
 }
